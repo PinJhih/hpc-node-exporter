49d46dfda44a491b0b9e13bef50338c588f29471
PinJhih <isaacwang29@gmail.com>
2024-07-19 15:28:44 +0800

    feat: Implement basic metrics exporter

---
+++ exporter.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "runtime"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+    cpuUsage = prometheus.NewGaugeVec(
+        prometheus.GaugeOpts{
+            Name: "cpu_usage_percentage",
+            Help: "CPU usage percentage per core",
+        },
+        []string{"core"},
+    )
+    memUsage = prometheus.NewGauge(
+        prometheus.GaugeOpts{
+            Name: "memory_usage_bytes",
+            Help: "Memory usage in bytes",
+        },
+    )
+)
+
+func init() {
+    prometheus.MustRegister(cpuUsage)
+    prometheus.MustRegister(memUsage)
+}
+
+func getMetrics() {
+    for i := 0; i < runtime.NumCPU(); i++ {
+        util := 0.0 // TODO: get CPU core utilization
+        cpuUsage.With(prometheus.Labels{"core": fmt.Sprintf("%d", i)}).Set(util)
+    }
+
+    memUsage.Set(0.0) // TODO: get RAM usage
+}
+
+func metricsHandler(w http.ResponseWriter, r *http.Request) {
+    getMetrics()
+    promhttp.Handler().ServeHTTP(w, r)
+}
+
+func main() {
+    http.HandleFunc("/metrics", metricsHandler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
